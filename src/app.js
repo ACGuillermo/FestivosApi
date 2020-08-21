520fc37c449002c7c560d3925e71a4ed293a29e3
GuillermoAguilar <39215353+ACGuillermo@users.noreply.github.com>
2020-08-21 14:40:30 +0200

    Added documentation page.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
@@ -14,6 +15,8 @@ app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname+'/public'))
+
 
 app.use('/api', api);
 
