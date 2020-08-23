ff586337476ad5e0c25b4600f2f88925f39eb915
GuillermoAguilar <39215353+ACGuillermo@users.noreply.github.com>
2020-08-23 19:55:19 +0200

    Added CCAA endpoints

--- src/api/ccaa.js
+++ src/api/ccaa.js
@@ -1,19 +1,43 @@
 const express = require('express');
+const monk = require('monk');
+
+// DB
+const db = monk(process.env.MONGO_URI);
+const ccaas = db.get('ccaa');
+
+// Loca cache - Not persistent
+const ccaaCache = new Map();
+const allCcaaCache = new Map();
 
 const router = express.Router();
 
 // Todos los festivos de todos las cca.
-router.get('/', (req, res, next) => {
-    res.json({
-        message: "hello ccaa"
-    })
+router.get('/', async(req, res, next) => {
+    if(allCcaaCache.has('all')) return res.json(allCcaaCache.get('all'))
+    try {
+        const items = await ccaas.find({}, '-_id');
+        res.json(items)
+        allCcaaCache.set('all', items)
+        db.close()
+    } catch (error) {
+        next(error)
+    }
 });
 
 // Todos los festivos de una cca.
-router.get('/:ccaa', (req, res, next) => {
-    res.json({
-        message: req.params.ccaa
-    })
+router.get('/:ccaa', async(req, res, next) => {
+    let {ccaa} = req.params;
+    if(ccaaCache.has(ccaa)) return res.json(ccaaCache.get(ccaa))
+    try {
+        const item = await ccaas.findOne({name: ccaa}, '-_id')
+        if(!item) return next()
+
+        res.json(item)
+        ccaaCache.set(ccaa, item)
+        db.close()
+    } catch (error) {
+        next(error)
+    }
 })
 
 module.exports = router;
